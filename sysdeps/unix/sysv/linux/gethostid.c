50938f1217c79f935f1632ef23c85b469ffe113a
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2018-01-29 08:11:11 -0200

    sethostid/gethostid

--- sysdeps/unix/sysv/linux/gethostid.c
+++ sysdeps/unix/sysv/linux/gethostid.c
@@ -19,56 +19,20 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <string.h>
+#include <sys/param.h>
+#include <resolv/netdb.h>
+#include <netinet/in.h>
 #include <not-cancel.h>
 
 #define HOSTIDFILE "/etc/hostid"
 
-#ifdef SET_PROCEDURE
-int
-sethostid (long int id)
-{
-  int fd;
-  ssize_t written;
-  int32_t id32 = id;
-
-  /* Test for appropriate rights to set host ID.  */
-  if (__libc_enable_secure)
-    {
-      __set_errno (EPERM);
-      return -1;
-    }
-
-  /* Make sure the ID is not too large.  Needed for bi-arch support.   */
-  if (id32 != id)
-    {
-      __set_errno (EOVERFLOW);
-      return -1;
-    }
-
-  /* Open file for writing.  Everybody is allowed to read this file.  */
-  fd = __open_nocancel (HOSTIDFILE, O_CREAT|O_WRONLY|O_TRUNC, 0644);
-  if (fd < 0)
-    return -1;
-
-  written = __write_nocancel (fd, &id32, sizeof (id32));
-
-  __close_nocancel_nostatus (fd);
-
-  return written != sizeof (id32) ? -1 : 0;
-}
-
-#else
-# include <string.h>
-# include <sys/param.h>
-# include <resolv/netdb.h>
-# include <netinet/in.h>
+#include <scratch_buffer.h>
 
 long int
 gethostid (void)
 {
   char hostname[MAXHOSTNAMELEN + 1];
-  size_t buflen;
-  char *buffer;
   struct hostent hostbuf, *hp;
   int32_t id;
   struct in_addr in;
@@ -93,25 +57,29 @@ gethostid (void)
     /* This also fails.  Return and arbitrary value.  */
     return 0;
 
-  buflen = 1024;
-  buffer = __alloca (buflen);
+  struct scratch_buffer buffer;
+  scratch_buffer_init (&buffer);
 
   /* To get the IP address we need to know the host name.  */
-  while (__gethostbyname_r (hostname, &hostbuf, buffer, buflen, &hp, &herr)
+  while (__gethostbyname_r (hostname, &hostbuf, buffer.data, buffer.length,
+			    &hp, &herr)
 	 != 0
 	 || hp == NULL)
-    if (herr != NETDB_INTERNAL || errno != ERANGE)
-      return 0;
-    else
-      /* Enlarge buffer.  */
-      buffer = extend_alloca (buffer, buflen, 2 * buflen);
+    {
+      if (herr != NETDB_INTERNAL || errno != ERANGE
+	  || !scratch_buffer_grow (&buffer))
+	{
+	  scratch_buffer_free (&buffer);
+	  return 0;
+	}
+    }
 
   in.s_addr = 0;
   memcpy (&in, hp->h_addr,
 	  (int) sizeof (in) < hp->h_length ? (int) sizeof (in) : hp->h_length);
+  scratch_buffer_free (&buffer);
 
   /* For the return value to be not exactly the IP address we do some
      bit fiddling.  */
   return (int32_t) (in.s_addr << 16 | in.s_addr >> 16);
 }
-#endif
--- sysdeps/unix/sysv/linux/sethostid.c
+++ sysdeps/unix/sysv/linux/sethostid.c
@@ -1,2 +1,57 @@
-#define SET_PROCEDURE 1
-#include "gethostid.c"
+/* Copyright (C) 1995-2018 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
+
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <not-cancel.h>
+
+#define HOSTIDFILE "/etc/hostid"
+
+int
+sethostid (long int id)
+{
+  int fd;
+  ssize_t written;
+  int32_t id32 = id;
+
+  /* Test for appropriate rights to set host ID.  */
+  if (__libc_enable_secure)
+    {
+      __set_errno (EPERM);
+      return -1;
+    }
+
+  /* Make sure the ID is not too large.  Needed for bi-arch support.   */
+  if (id32 != id)
+    {
+      __set_errno (EOVERFLOW);
+      return -1;
+    }
+
+  /* Open file for writing.  Everybody is allowed to read this file.  */
+  fd = __open_nocancel (HOSTIDFILE, O_CREAT|O_WRONLY|O_TRUNC, 0644);
+  if (fd < 0)
+    return -1;
+
+  written = __write_nocancel (fd, &id32, sizeof (id32));
+
+  __close_nocancel_nostatus (fd);
+
+  return written != sizeof (id32) ? -1 : 0;
+}
