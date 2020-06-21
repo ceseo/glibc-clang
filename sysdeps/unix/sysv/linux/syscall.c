c8a7456747d5e3ef7ee4a00ebc17ffc992b36840
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-06-21 15:03:25 -0300

    linux: Add generic C syscall implementation
    The hppa and riscv one are also removed.  The riscv specially is
    abusing the internal interfaces by defining it with a different
    signature.  If the idea is to provide an optimized version a better
    alternative would be to provide an assembly one as other
    architectures.
    
    Checked on x86_64-linux-gnu and with build for the affected ABIs.

--- sysdeps/unix/sysv/linux/syscall.c
+++ sysdeps/unix/sysv/linux/syscall.c
@@ -1,5 +1,5 @@
-/* system call interface.  Linux/RISC-V version.
-   Copyright (C) 2001-2020 Free Software Foundation, Inc.
+/* Indirect system call.  Linux version.
+   Copyright (C) 2020 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -16,19 +16,26 @@
    License along with the GNU C Library.  If not, see
    <https://www.gnu.org/licenses/>.  */
 
+#include <stdarg.h>
 #include <sysdep.h>
 
 long int
-syscall (long int syscall_number, long int arg1, long int arg2, long int arg3,
-	 long int arg4, long int arg5, long int arg6, long int arg7)
+syscall (long int number, ...)
 {
-  long int ret;
+  va_list args;
 
-  ret = INTERNAL_SYSCALL_NCS (syscall_number, 7, arg1, arg2, arg3, arg4,
-			      arg5, arg6, arg7);
+  va_start (args, number);
+  long int arg0 = va_arg (args, long int);
+  long int arg1 = va_arg (args, long int);
+  long int arg2 = va_arg (args, long int);
+  long int arg3 = va_arg (args, long int);
+  long int arg4 = va_arg (args, long int);
+  long int arg5 = va_arg (args, long int);
+  va_end (args);
 
-  if (INTERNAL_SYSCALL_ERROR_P (ret))
-    return __syscall_error (ret);
-
-  return ret;
+  long int ret = INTERNAL_SYSCALL_NCS (number, 6, arg0, arg1, arg2,
+				       arg3, arg4, arg5);
+  return INTERNAL_SYSCALL_ERROR_P (ret)
+	 ? SYSCALL_ERROR_LABEL (INTERNAL_SYSCALL_ERRNO (ret))
+	 : ret;
 }
