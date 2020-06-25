f0db97cdf71e387bf89e88af82f3cf53c9c4bfc7
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2020-06-25 17:56:54 -0300

    alpha: Move implementations to C
    Move alpha specific the feraiseexcept, getegid, geteuid, getppid,
    eee_get_fp_control, ieee_set_fp_control, and setcontext to C
    implementations.
    
    Checked on alpha-linux-gnu.

---
+++ sysdeps/unix/sysv/linux/alpha/getegid.c
@@ -0,0 +1,27 @@
+/* Get effective group identity.  Linux/Alpha.
+   Copyright (C) 2020 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library.  If not, see
+   <https://www.gnu.org/licenses/>.  */
+
+#include <unistd.h>
+#include <sysdep.h>
+
+gid_t
+__getegid (void)
+{
+  return __internal_syscall_pair (__NR_getxgid).sc_20;
+}
+weak_alias (__getegid, getegid)
