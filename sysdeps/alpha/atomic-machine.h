1e06ed9c94735f4590caa7bf5bc0cb391ac0ef4b
Adhemerval Zanella <adhemerval.zanella@linaro.org>
2022-06-24 10:51:56 -0300

    alpha: Enable USE_ATOMIC_COMPILER_BUILTINS

--- sysdeps/alpha/atomic-machine.h
+++ sysdeps/alpha/atomic-machine.h
@@ -18,7 +18,7 @@
 #include <stdint.h>
 
 #define __HAVE_64B_ATOMICS 1
-#define USE_ATOMIC_COMPILER_BUILTINS 0
+#define USE_ATOMIC_COMPILER_BUILTINS 1
 
 /* XXX Is this actually correct?  */
 #define ATOMIC_EXCHANGE_USES_CAS 1
